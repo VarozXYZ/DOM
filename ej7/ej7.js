63df25055f0da9610999aa5a039e4af8056be9a2
VarozXYZ <alvaropiro@gmail.com>
2025-10-20 11:04:17 +0200

    ejercicio 6 - display = block

---
+++ ej7/ej7.js
@@ -0,0 +1,10 @@
+const frame = document.getElementById("frame")
+const btnCreate = document.getElementById("btnCreate");
+
+let boxCreated = false;
+
+btnCreate.addEventListener("click", () => {
+    if (!boxCreated) {
+        const box = document.createElement("div");
+    }
+})
