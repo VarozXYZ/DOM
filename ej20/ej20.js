c17521a3f7ea0ee6af993266ce7a871fe8e66c1d
VarozXYZ <alvaropiro@gmail.com>
2025-10-27 09:38:05 +0100

    ej20

---
+++ ej20/ej20.js
@@ -0,0 +1,57 @@
+const form = document.getElementById('form');
+const field1 = document.getElementById("field1")
+const field2 = document.getElementById("field2")
+const tbody = document.getElementById("tbody")
+let currentID = 1;
+
+form.addEventListener('submit', (e) => {
+    e.preventDefault();
+
+    const tr = document.createElement('tr')
+
+    const tdId = document.createElement('td')
+    tdId.textContent = currentID;
+
+    const td1 = document.createElement('td')
+    td1.textContent = field1.value;
+
+    const td2 = document.createElement('td')
+    td2.textContent = field2.value;
+
+    tr.append(tdId, td1, td2)
+    tbody.appendChild(tr)
+
+    const tdActions = document.createElement('td');
+
+    const actionsDiv = document.createElement('div')
+    actionsDiv.className = 'actions-buttons'
+    const btnEdit = document.createElement('button')
+    btnEdit.textContent = "Edit";
+    btnEdit.className = "btn-edit";
+
+    btnEdit.addEventListener("click", (e) => {
+        const newValue1 = prompt("Editar campo 1:", td1.textContent);
+        const newValue2 = prompt("Editar campo 2:", td2.textContent);
+
+        if (newValue1 !== null && newValue1.trim() !== '') {
+            td1.textContent = newValue1;
+        }
+        if (newValue2 !== null && newValue2.trim() !== '') {
+            td2.textContent = newValue2;
+        }
+    })
+
+    const btnDelete = document.createElement('button')
+    btnDelete.textContent = 'Delete';
+    btnDelete.className = 'btn-delete';
+    btnDelete.addEventListener('click', (e) => {
+        tbody.removeChild(tr)
+    })
+
+    actionsDiv.append(btnDelete, btnEdit);
+    tdActions.appendChild(actionsDiv);
+    tr.append(tdId, td1, td2, tdActions);
+    tbody.appendChild(tr);
+    currentID++;
+    form.reset();
+})
